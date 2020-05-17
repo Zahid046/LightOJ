897f43aaa29a43cb3fb6e61cf48b5cf77f905b2a
MD Zahid Mahmud Emon <52098073+Zahid046@users.noreply.github.com>
2020-05-18 02:12:17 +0600

    Update 1000 - Greetings from LightOJ.cpp

--- Beginners Problems/1000 - Greetings from LightOJ.cpp
+++ Beginners Problems/1000 - Greetings from LightOJ.cpp
@@ -1,1 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+#define ll long long
+
+int main()
+{
+    ll a,b,t;
+    cin>>t;
+    for(int i=0; i<t; i++){
+        cin>>a>>b;
+        ll s=a+b;
+        cout<<"Case "<<i+1<<": "<<s<<endl;
+    }
+}
